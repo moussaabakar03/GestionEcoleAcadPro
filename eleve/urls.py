fd5eac4c6fa78626c2e63e96254b7f3facdaf9cf
Grace925852 <essolotiegraceatta@gmail.com>
2025-04-07 19:20:36 +0000

    mise à jour eleve

---
+++ eleve/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index3/', views.index3, name='index3'),
+    path('index4/', views.index4, name='index4'),
+    path('index5/', views.index5, name='index5'),
+
+    path('admit-form/', views.admit_form, name='admit-form'),
+
+    path('button/', views.button, name='button'),
+
+    path('class-routine/', views.class_routine, name='class-routine'),
+
+    path('exam-grade/', views.exam_grade, name='exam-grade'),
+
+    path('grid/', views.grid, name='grid'),
+
+    path('messaging/', views.messaging, name='messaging'),
+
+    path('modal/', views.modal, name='modal'),
+
+    path('notice-board/', views.notice_board, name='notice-board'),
+
+    path('notification-alart/', views.notification_alart, name='notification-alart'),
+
+    path('progress-bar/', views.progress_bar, name='progress-bar'),
+    
+]
--- eleve/views.py
+++ eleve/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def index3(request):
+    return render(request, 'index3.html')
+
+def index4(request):
+    return render(request, 'index4.html')
+
+def index5(request):
+    return render(request, 'index5.html')
+
+def admit_form(request):
+    return render(request, 'admit-form.html')
+
+def button(request):
+    return render(request, 'button.html')
+
+def class_routine(request):
+    return render(request, 'class-routine.html')
+
+def exam_grade(request):
+    return render(request, 'exam-grade.html')
+
+def grid(request):
+    return render(request, 'grid.html')
+
+def messaging(request):
+    return render(request, 'messaging.html')
+
+def modal(request):
+    return render(request, 'modal.html')
+
+def notice_board(request):
+    return render(request, 'notice-board.html')
+
+def notification_alart(request):
+    return render(request, 'notification-alart.html')
+
+def progress_bar(request):
+    return render(request, 'progress-bar.html')
